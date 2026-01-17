1a6b2cdc1cb5a1e5115b174fa38acb5b2719ec13
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-17 23:21:56 +0100

    feature: provide fluent interface pattern

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,10 +13,7 @@ fun main() {
     }
     "push" example {
         val list = LinkedList<Int>()
-        list.push(3)
-        list.push(2)
-        list.push(1)
-
+        list.push(3).push(2).push(1)
         println(list)
     }
 }
