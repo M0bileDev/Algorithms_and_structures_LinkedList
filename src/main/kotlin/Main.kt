05c106d33bd5260614e39bf195c3e860d39ea602
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-20 21:11:04 +0100

    feature: provide pop implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -37,6 +37,17 @@ fun main() {
         println("After inserting: $list")
     }
 
+    "pop" example {
+        val list = LinkedList<Int>()
+        list.push(3)
+        list.push(2)
+        list.push(1)
+
+        println("Before popping list: $list")
+        val poppedValue = list.pop()
+        println("After popping list: $list")
+        println("Popped value: $poppedValue")
+    }
 }
 
 infix fun String.example(function: () -> Unit) {
