fca8ad7e5f4f26c1456337ff724fac2889c1b2bb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-22 22:54:03 +0100

    feature: provide implementations and create test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -89,6 +89,44 @@ fun main() {
         }
     }
 
+    "removing elements" example {
+        val list: MutableCollection<Int> = LinkedList()
+        list.add(3)
+        list.add(2)
+        list.add(1)
+
+        println(list)
+        list.remove(1)
+        println(list)
+    }
+
+    "retaining elements" example {
+        val list: MutableCollection<Int> = LinkedList()
+        list.add(3)
+        list.add(2)
+        list.add(1)
+        list.add(4)
+        list.add(5)
+
+        println(list)
+        list.retainAll(listOf(3, 4, 5))
+        println(list)
+    }
+
+    "remove all elements" example {
+        val list: MutableCollection<Int> = LinkedList()
+        list.add(3)
+        list.add(2)
+        list.add(1)
+        list.add(4)
+        list.add(5)
+
+        println(list)
+        list.removeAll(listOf(3, 4, 5))
+        println(list)
+    }
+
+
 }
 
 infix fun String.example(function: () -> Unit) {
