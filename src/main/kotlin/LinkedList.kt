4e4d3169bda7c064f0de9118e027369a7fc206e4
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-22 21:53:49 +0100

    feature: provide collection interface
    Implemented collection interface methods

--- src/main/kotlin/LinkedList.kt
+++ src/main/kotlin/LinkedList.kt
@@ -1,12 +1,12 @@
 package org.example
 
-class LinkedList<T : Any> : Iterable<T> {
+class LinkedList<T : Any> : Iterable<T>, Collection<T> {
     private var head: Node<T>? = null
     private var tail: Node<T>? = null
-    var size = 0
+    override var size = 0
         private set
 
-    fun isEmpty(): Boolean = size == 0
+    override fun isEmpty(): Boolean = size == 0
 
     override fun toString(): String {
         return if (isEmpty()) {
@@ -115,5 +115,20 @@ class LinkedList<T : Any> : Iterable<T> {
 
     override fun iterator(): Iterator<T> {
         return LinkedListIterator(this)
+    }
+
+    override fun contains(element: T): Boolean {
+        for (item in this) {
+            if (item == element) return true
+        }
+        return false
+    }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        for (searched in elements) {
+            if (!contains(searched)) return false
+        }
+
+        return true
     }
 }
