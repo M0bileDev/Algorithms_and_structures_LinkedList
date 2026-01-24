3961318521170683ca67f2b5df36d5d398ecf742
m0bileDev <damian.ogorek.1996@gmail.com>
2026-01-24 21:30:46 +0100

    feature: provide extension function that finds for the middle node

--- src/main/kotlin/LinkedListExt.kt
+++ src/main/kotlin/LinkedListExt.kt
@@ -1,11 +1,19 @@
 package org.example
 
-fun <T: Any> LinkedList<T>.printInReverse(){
+fun <T : Any> LinkedList<T>.printInReverse() {
     var counter = size - 1
-    while (counter > -1){
-       val node = nodeAt(counter)
+    while (counter > -1) {
+        val node = nodeAt(counter)
         print(node?.value)
         counter--
-        if(counter > -1) print(" <- ")
+        if (counter > -1) print(" <- ")
     }
+}
+
+fun <T : Any> LinkedList<T>.findItemInTheMiddle(): Node<T>? {
+    if (isEmpty()) return null
+    if (size == 1) return nodeAt(0)
+
+    val middleIndex = size / 2
+    return nodeAt(middleIndex)
 }
